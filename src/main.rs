d6da4881eb2a88e37a4f3b2a5b68722cab29decb
Michael Rutter <michael.john.rutter@gmail.com>
2018-03-14 22:50:15 +0000

    reorganisation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 extern crate qlik_rs;
+extern crate serde;
+extern crate serde_json;
 extern crate ws;
 
+use qlik_rs::{QHyperCubeDef, UrlBuilder};
+
 fn main() {
-    let a = qlik_rs::build_url::UrlBuilder::new();
+    let a = UrlBuilder::new();
+
+    let b = QHyperCubeDef::new();
 
     println!("{:?}", a);
+    println!("{:?}", serde_json::to_string(&b).unwrap());
 }
