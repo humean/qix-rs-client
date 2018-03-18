cdcbd65ecc68ec5d96d5ac6d8ea6b2d2e9bce3b7
Michael Rutter <michael.john.rutter@gmail.com>
2018-03-18 14:01:17 +0000

    writing tests are good, but next time write them before implmentation

--- src/build_url.rs
+++ src/build_url.rs
@@ -1,6 +1,6 @@
 use std::borrow::Borrow;
 use url::Url;
-use url::percent_encoding::{utf8_percent_encode, DEFAULT_ENCODE_SET};
+use url::percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
 use failure::Error;
 use regex::Regex;
 
@@ -19,7 +19,7 @@ use regex::Regex;
 ///     .build()
 ///     .unwrap();
 ///
-///  assert!(url == "wss://localhost:4848/")
+///  assert_eq!(url, "wss://localhost:4848/")
 ///
 /// ```
 #[derive(Default, Debug, Clone)]
@@ -99,62 +99,62 @@ impl UrlBuilder {
     }
 
     /// Sets the hostname
-    pub fn with_hostname(mut self, hostname: &str) -> Self {
+    pub fn with_hostname(&mut self, hostname: &str) -> &mut Self {
         self.host = strip_leading_trailing_slashes(hostname);
         self
     }
 
     /// Sets whether to us secure WebSockets. Default false
-    pub fn with_secure(mut self, secure: bool) -> Self {
+    pub fn with_secure(&mut self, secure: bool) -> &mut Self {
         self.secure = secure;
         self
     }
 
     /// Sets the port, with the default being the default WebSocket ports
-    pub fn with_port(mut self, port: u32) -> Self {
+    pub fn with_port(&mut self, port: u32) -> &mut Self {
         self.port = Some(port);
         self
     }
 
     /// Sets the ID of the app intended to be opened in the session
-    pub fn with_app_id(mut self, app_id: &str) -> Self {
-        self.app_id = Some(utf8_percent_encode(app_id, DEFAULT_ENCODE_SET).to_string());
+    pub fn with_app_id(&mut self, app_id: &str) -> &mut Self {
+        self.app_id = Some(utf8_percent_encode(app_id, PATH_SEGMENT_ENCODE_SET).to_string());
         self
     }
 
     /// Sets a base absolute path when connecting, to be used with Qlik proxy prefix
-    pub fn with_prefix(mut self, prefix: &str) -> Self {
+    pub fn with_prefix(&mut self, prefix: &str) -> &mut Self {
         self.prefix = Some(strip_leading_trailing_slashes(prefix));
         self
     }
 
     /// Set an identity (session ID) to use
-    pub fn with_identity(mut self, identity: &str) -> Self {
-        self.identity = Some(utf8_percent_encode(identity, DEFAULT_ENCODE_SET).to_string());
+    pub fn with_identity(&mut self, identity: &str) -> &mut Self {
+        self.identity = Some(utf8_percent_encode(identity, PATH_SEGMENT_ENCODE_SET).to_string());
         self
     }
 
     /// Sets the subpath to use. Used to connect to dataprepservice in a server environment
-    pub fn with_subpath(mut self, subpath: &str) -> Self {
+    pub fn with_subpath(&mut self, subpath: &str) -> &mut Self {
         self.subpath = Some(strip_leading_trailing_slashes(subpath));
         self
     }
 
     /// Sets initial route to open the WebSocket against
-    pub fn with_route(mut self, route: &str) -> Self {
+    pub fn with_route(&mut self, route: &str) -> &mut Self {
         self.route = Some(strip_leading_trailing_slashes(route));
         self
     }
 
     /// A value in seconds that QIX Engine should keep the session alive after
     /// socket disconnect (only works if QIX Engine supports it)
-    pub fn with_ttl(mut self, ttl: u32) -> Self {
+    pub fn with_ttl(&mut self, ttl: u32) -> &mut Self {
         self.ttl = Some(ttl);
         self
     }
 
     /// Sets additional parameters to be added to WebSocket URL
-    pub fn with_params<T, K, V>(self, _url_params: T) -> Self
+    pub fn with_params<T, K, V>(&mut self, _url_params: T) -> &mut Self
     where
         T: IntoIterator,
         T::Item: Borrow<(K, V)>,
@@ -170,4 +170,67 @@ fn strip_leading_trailing_slashes(s: &str) -> String {
     let re = Regex::new("(^[/]+)|([/]+$)").unwrap();
 
     re.replace_all(s, "").into_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_options_set_returns_default_url() {
+        let url = UrlBuilder::new().build().unwrap();
+        assert_eq!(url, "ws://localhost/");
+    }
+
+    #[test]
+    fn works_with_various_hostnames_types() {
+        let mut builder = UrlBuilder::new();
+
+        let url = builder
+            .with_hostname("subdomain.example.com")
+            .build()
+            .unwrap();
+
+        assert_eq!(url, "ws://subdomain.example.com/");
+
+        let url = builder.with_hostname("127.0.0.0").build().unwrap();
+
+        assert_eq!(url, "ws://127.0.0.0/");
+
+        let url = builder
+            .with_hostname("[2001:0db8:0a0b:12f0:0000:0000:0000:0001]")
+            .build()
+            .unwrap();
+
+        assert_eq!(url, "ws://[2001:db8:a0b:12f0::1]/");
+
+        let url = builder.with_hostname("").build().unwrap();
+
+        assert_eq!(url, "ws://localhost/");
+    }
+
+    #[test]
+    fn trailing_input_is_removed() {
+        let url = UrlBuilder::new()
+            .with_hostname("example.com/////////////")
+            .with_prefix("/mobile///")
+            .build()
+            .unwrap();
+
+        assert_eq!(url, "ws://example.com/mobile")
+    }
+
+    #[test]
+    fn app_id_encoded_correctly() {
+        let url = UrlBuilder::new()
+            .with_app_id("My App File Name With_Some%Annoying Stuff!InItsName?")
+            .build()
+            .unwrap();
+
+        assert_eq!(
+            url,
+            "ws://localhost/app/My%20App%20File%20Name%20With_Some%25Annoying%20Stuff!InItsName%3F"
+        )
+    }
+
 }
